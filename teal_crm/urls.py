dc622323931f7bf0bdb6f7c8102b41cacc1de72f
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-12-26 14:01:04 +0100

    inital commit
    ccreated a new project and setup basic templates and routing for the project
    
    created userprofile app to manage authentication with login, signup, logout functionality
    
    base template was styles using tailwindcss cdn in  the core app
    
    the core app is the base of the project

---
+++ teal_crm/urls.py
@@ -0,0 +1,30 @@
+"""teal_crm URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+
+from core.views import index, about
+from userprofile.views import signup
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('about/', about, name='about'),
+    path('sign-up/', signup, name='signup'),
+    path('log-in/', LoginView.as_view(template_name='userprofile/login.html'), name='login'),
+    path('log-out/', LogoutView.as_view(), name='logout'),
+    path('admin/', admin.site.urls),
+]
---
+++ userprofile/views.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+from .models import UserProfile
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            UserProfile.objects.create(user=user)
+
+            return redirect('/log-in/')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'userprofile/signup.html',{
+        'form': form
+    })
